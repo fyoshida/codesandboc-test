f1c5d0755c5cdaa5e15aba72d5abbce4e09705ca
fyoshida <fyoshida2003@gmail.com>
2021-08-18 02:36:46 +0900

    論理演算子の意味

--- src/index.js
+++ src/index.js
@@ -177,6 +177,30 @@ const nameArr = ["田中", "佐藤", "吉田"];
 // const formattedNum =  typeof num==='number' ? num.toLocaleString : '数値を入力してください';
 // console.log(formattedNum)
 
-const checkSum = (num1, num2) =>
-  num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
-console.log(checkSum(30, 71));
+// const checkSum = (num1, num2) =>
+//   num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// console.log(checkSum(30, 71));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+
+// const flag1 = true;
+// const flag2 = true;
+// if (flag1 && flag2) {
+//   console.log("1も2はtrueになります");
+// }
+
+// || は左側がfalseなら右を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+const ff = (n1, n2) => (n1 ? n1 : n2);
+console.log(ff(10, 20));
+console.log(ff(null, 20));
+
+// && は左側がtrueなら右を返す
+const num2 = null;
+const fee2 = num2 && "何か設定してください";
+console.log(fee2);
